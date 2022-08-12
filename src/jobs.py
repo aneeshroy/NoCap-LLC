7c5d49def01c51b60f0e3b9b60686a08b2186829
aneeshroy <78515042+aneeshroy@users.noreply.github.com>
2022-08-12 12:55:37 -0500

    Update jobs.py

--- src/jobs.py
+++ src/jobs.py
@@ -6,9 +6,7 @@ import redis
 from hotqueue import HotQueue
 
 
-redis_ip = os.environ.get('REDIS_IP')
-if not redis_ip:
-    raise Exception()
+redis_ip = 127.0. 0.1
 
 rd = redis.Redis(host=redis_ip, port=6379, db=0, decode_responses=True)
 q = HotQueue("queue", host=redis_ip, port=6379, db=1)
